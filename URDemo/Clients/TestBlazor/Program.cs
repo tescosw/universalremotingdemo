dd915cf69a48477c391ca3dcbe0e1c5e9e4e27c4
Sekáč Ondřej <Ondrej.Sekac@tescosw.cz>
2024-01-09 10:31:28 +0100

    blazor demo

--- URDemo/Clients/TestBlazor/Program.cs
+++ URDemo/Clients/TestBlazor/Program.cs
@@ -13,5 +13,6 @@ builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.
 builder.Services.Add(ServiceDescriptor.Singleton<IHTTPCommunicator>((sc) => new SingleUserCommunicator(builder.HostEnvironment.BaseAddress)));
 builder.Services.AddScopedOWAuthentication();
 builder.Services.AddScopedOWManager();
+builder.Services.AddScopedServiceInfo();
 
 await builder.Build().RunAsync();
